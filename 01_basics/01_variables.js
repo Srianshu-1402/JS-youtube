a541a23a24b7c6362d94fae1c7d100de55f137dd
Shriyanshu <sheri112@gmail.com>
2025-08-25 12:30:12 +0530

    datatypes & variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "ssr@1402gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+accountEmail = "hc@hv.com"
+accountPassword = "97097090"
+accountCity = "mumbai"
+
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState])
---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,16 @@
+// Data Types in JavaScript
+/*
+1. Primitive Data Types:
+   - Number = 2 to the power 53
+   - String = "asdf"
+   - Boolean = true/false
+   - Undefined = undefined
+   - Null = null
+   - Symbol (ES6) = Symbol()
+
+2. Reference Data Types:
+   - Object
+   - Array
+   - Function
+*/
+console.log(typeof string);
